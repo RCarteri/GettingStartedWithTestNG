03eb936f70c97641d590698294b9ab4acd2aa70d
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-07-21 16:29:31 -0300

    5. Api tests with data providers

---
+++ src/test/java/api/ApiTestWithDataProviders.java
@@ -0,0 +1,51 @@
+package api;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertEquals;
+
+public class ApiTestWithDataProviders {
+
+    private CloseableHttpClient client;
+    private CloseableHttpResponse response;
+
+    @BeforeMethod
+    public void buildClient(){
+        // Arrange
+        client = HttpClientBuilder.create().build();
+    }
+
+    @DataProvider
+    public static Object[][] endpointsRequiringAuthentication(){
+        return new Object[][]{
+                {"user"},
+                {"user/followers"},
+                {"notifications"}
+        };
+    }
+
+    @Test(dataProvider = "endpointsRequiringAuthentication")
+    public void userEndpointReturns401(String endpoint) throws IOException {
+        // Act
+        response = client.execute(new HttpGet("https://api.github.com/" + endpoint));
+        int actualStatusCode = response.getStatusLine().getStatusCode();
+
+        // Assert
+        assertEquals(actualStatusCode, 401);
+    }
+
+    @AfterMethod
+    public void cleanup() throws IOException {
+        client.close();
+        response.close();
+    }
+}
