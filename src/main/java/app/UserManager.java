128348d02a015211f5b1c8e84568a335d7d213fb
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-07-21 16:18:09 -0300

    5. Unit tests with DataProviders

--- src/main/java/app/UserManager.java
+++ src/main/java/app/UserManager.java
@@ -1,16 +1,20 @@
 package app;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class UserManager {
-    private Collection<String> userStorage;
+    private final Collection<String> userStorage;
 
     public UserManager() {
         this.userStorage = new ArrayList<>();
     }
 
     public boolean addUser(String userEmail) throws DuplicateUserException{
+        if (!EmailValidator.getInstance().isValid(userEmail))
+            throw new IllegalArgumentException("Not a valid email");
         for (String s : userStorage) {
             if (s.equals(userEmail)) {
                 throw new DuplicateUserException("User already exists");
