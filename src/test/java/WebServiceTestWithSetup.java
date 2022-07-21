5b9b7868c8c5fe264a40503ae48d2386df818187
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-07-21 10:03:12 -0300

    3. The skipException

--- src/test/java/WebServiceTestWithSetup.java
+++ src/test/java/WebServiceTestWithSetup.java
@@ -1,36 +1,9 @@
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.testng.annotations.*;
-
-import java.io.IOException;
+import org.testng.annotations.Test;
 
 import static org.apache.http.entity.ContentType.getOrDefault;
 import static org.testng.Assert.assertEquals;
 
-public class WebServiceTestWithSetup {
-    CloseableHttpClient client;
-    CloseableHttpResponse response;
-
-    @BeforeClass
-    public void setup() throws IOException {
-        System.out.println("Runs once per class");
-        client = HttpClientBuilder.create().build();
-        response = client.execute(new HttpGet("https://api.github.com/"));
-    }
-
-    @BeforeMethod
-    public void setupMethod() {
-        System.out.println("Runs before each @Test");
-    }
-
-    @AfterClass
-    public void cleanup() throws IOException {
-        client.close();
-        response.close();
-    }
-
+public class WebServiceTestWithSetup extends WebServiceBaseClass{
     @Test
     public void statusIs200() {
         assertEquals(response.getStatusLine().getStatusCode(), 200);
