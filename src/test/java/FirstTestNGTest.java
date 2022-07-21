ad844acd9711a46fb7141950d5c88db6ac60b93e
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-07-21 10:14:39 -0300

    4. Description

--- src/test/java/FirstTestNGTest.java
+++ src/test/java/FirstTestNGTest.java
@@ -1,26 +1,30 @@
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Method;
+
 import static org.testng.Assert.*;
 
 public class FirstTestNGTest extends UnitTestBaseClass{
     UserManager um;
 
     @BeforeMethod
-    public void customLocalsetupMethod() {
+    public void customLocalsetupMethod(Method testMethod) {
+        String desc = testMethod.getAnnotation(Test.class).description();
+        System.out.println("Starting test: " + testMethod.getName() + "with descripption: " + desc);
+//        Arrange
         um = new UserManager();
     }
 
-    @Test
+    @Test(description = "Verify that addUser method returns true when successful")
     private void successfulAddUserReturnsTrue() {
-//    Arrange
 //    Act
         boolean result = um.addUser("ricardo@gmail.com");
 //    Assert
         assertTrue(result);
     }
 
-    @Test
+    @Test(description = "Verify that getUser method retrieves the correct existing user")
     private void getUserReturnsExistingSavedUser() {
 //    Arrange
         um.addUser("ricardo@gmail.com");
@@ -30,9 +34,8 @@ public class FirstTestNGTest extends UnitTestBaseClass{
         assertEquals(user, "ricardo@gmail.com");
     }
 
-    @Test
+    @Test(description = "Verify that getUser method returns null if the user doesn't exist")
     private void getNonExixtingUserReturnsNull() {
-//    Arrange
 //    Act
         String user = um.getUser("ricardo@gmail.com");
 //    Assert
